59c328c1a3e289df44e3ec7ac6a2f9d94c0eaba8
Jake <JakeShoffner27@gmail.com>
2023-04-10 14:36:42 -0400

    fixed a little of sidebar and homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Typed from 'react-typed';
 
 const Hero = () => {
   return (
     <div className='w-full pt-[70px] h-screen md:pt-[0vh] flex justify-between items-center flex-col text-white'>
-      <div className='flex justify-center flex-col items-center w-full h-[35vh] bg-red-300 max-w-[1500px]'>
-        <h1>Jake Shoffner</h1>
-        <h1>SWE Student</h1>
-      </div>
+      <div className='flex justify-center flex-col items-center w-full h-[35vh] max-w-[1500px] md:text-5xl text-4xl font-bold text-black'>
+        
+        <div className='drop-shadow-2xl flex flex-col justify-center items-center'>
+          <h1>Jake Shoffner</h1>
+          <Typed className="md:text-3xl text-2xl font-semibold" strings={['software engineer...', 'problem solver...', 'self-proclaimed comedian...']} typeSpeed={65} loop backDelay={1800} backSpeed={50}/>
+        </div>
+
+       </div>
 
       <div className='grid md:grid-rows-1 h-[65vh] bg-red-400 max-w-[1500px] grid-flow-col w-full grid-rows-2'>
         <div className='bg-blue-300'>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,35 +4,21 @@ import { Icon } from '@iconify/react';
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(true);
+  const nav = "NAVIGATION";
+  const navArray = nav.split("");
 
   return (
-    <div className={`hidden duration-300 bg-[#121212] text-white drop-shadow-2xl h-screen w- relative md:flex justify-center ${sidebar ? 'w-[0vw] md:w-[50px]' : 'w-[0vw] md:w-[185px]'}`}>
-        <div className='top-[150px] absolute'>
-          <div className={`w-full h-1/2 flex flex-col select-none ${sidebar ? 'text-[0px]' : 'text-lg font-semibold'}`}>
-            <h1 className='my-6 hover:scale-105 ease-linear duration-150'><span className='text-[#1db954]'>0000: </span>About</h1>
-            <h1 className='my-6 hover:scale-105 ease-linear duration-150'><span className='text-[#1db954]'>0001: </span>Experience</h1>
-            <h1 className='my-6 hover:scale-105 ease-linear duration-150'><span className='text-[#1db954]'>0010: </span>Projects</h1>
-            <h1 className='my-6 hover:scale-105 ease-linear duration-150'><span className='text-[#1db954]'>0011: </span>Skills</h1>
-            <h1 className='my-6 hover:scale-105 ease-linear duration-150'><span className='text-[#1db954]'>0100: </span>Contact</h1>
-          </div>
-        </div>
-
+    <div className={`hidden duration-300 bg-[#121212] text-white drop-shadow-2xl h-screen w- relative md:flex justify-center md:w-[185px] ${sidebar ? 'ml-[-133px]' : ''}`}>
+        {/* This is the click-able icon that opens the sidebar */}
         <div className='right-1.5 duration-300 absolute text-[#1db954] font-bold top-[50px] hover:text-[#188b40] hover:rotate-90'>
             <h1 className='cursor-pointer' onClick={() => setSidebar(!sidebar)}><Icon className='text-[35px]' icon="game-icons:multi-directions" /></h1>
         </div>
 
-      <div className={`absolute top-[150px] select-none items-center flex flex-col text-xl ${sidebar ? '' : 'opacity-0'}`}>
-        <h1>N</h1>
-        <h1>A</h1>
-        <h1>V</h1>
-        <h1>I</h1>
-        <h1>G</h1>
-        <h1>A</h1>
-        <h1>T</h1>
-        <h1>I</h1>
-        <h1>O</h1>
-        <h1>N</h1>
+      {/* This is the click-able "NAVBAR" horizontal text along the sidebar... it also opens the sidebar*/} 
+      <div onClick={() => setSidebar(!sidebar)} className={`absolute top-[150px] cursor-pointer select-none hover:scale-105 duration-300 items-center flex flex-col text-xl right-4 ${sidebar ? '' : 'ml-[-133px]'}`}>
+        {navArray.map((x, index) => <h1 key={index}>{x}</h1>)}
       </div>
+
     </div>
   )
 }
