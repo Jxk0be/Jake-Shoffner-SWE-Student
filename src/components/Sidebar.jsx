d0c5db2b5e736a2bfd49c01b23c367586dd36a11
Jake <JakeShoffner27@gmail.com>
2023-04-11 00:32:30 -0400

    Finished sidebar other than the routing and some small stylistic changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,14 +8,43 @@ const Navbar = () => {
   const navArray = nav.split("");
 
   return (
-    <div className={`hidden duration-300 bg-[#121212] text-white drop-shadow-2xl h-screen w- relative md:flex justify-center md:w-[185px] ${sidebar ? 'ml-[-133px]' : ''}`}>
-        {/* This is the click-able icon that opens the sidebar */}
-        <div className='right-1.5 duration-300 absolute text-[#1db954] font-bold top-[50px] hover:text-[#188b40] hover:rotate-90'>
-            <h1 className='cursor-pointer' onClick={() => setSidebar(!sidebar)}><Icon className='text-[35px]' icon="game-icons:multi-directions" /></h1>
+    <div className={`hidden duration-300 bg-[#121212] text-white drop-shadow-2xl h-screen w- relative md:flex justify-center md:w-[250px] ${sidebar ? 'ml-[-196px]' : ''}`}>
+      {/* This is the click-able icon that opens the sidebar */}
+      <div className='right-1.5 duration-300 absolute hover:text-[#b3b3b3] font-bold top-[50px] text-[#ffffff] hover:rotate-90'>
+          <h1 className='cursor-pointer' onClick={() => setSidebar(!sidebar)}><Icon className='text-[35px]' icon="game-icons:multi-directions" /></h1>
+      </div>
+
+      <div className='absolute top-[152px] ml-[-30px] left-4 flex justify-between flex-col h-[280px]'>
+
+        <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-[#535353] py-2 rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+          <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="material-symbols:verified" />
+          <h1>About Me</h1>
         </div>
 
+        <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-[#535353] py-2 rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+          <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="cryptocurrency:xp" />
+          <h1>Experience</h1>
+        </div>
+
+        <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-[#535353] py-2 rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+          <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="file-icons:ionic-project" />
+          <h1>Projects</h1>
+        </div>
+
+        <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-[#535353] py-2 rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+          <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="mdi:email-open" />
+          <h1>Email</h1>
+        </div>
+
+        <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-[#535353] py-2 rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+          <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="uil:linkedin" />
+          <h1>Linkedin</h1>
+        </div>
+
+      </div>
+
       {/* This is the click-able "NAVBAR" horizontal text along the sidebar... it also opens the sidebar*/} 
-      <div onClick={() => setSidebar(!sidebar)} className={`absolute top-[150px] cursor-pointer select-none hover:scale-105 duration-300 items-center flex flex-col text-xl right-4 ${sidebar ? '' : 'ml-[-133px]'}`}>
+      <div onClick={() => setSidebar(!sidebar)} className={`absolute bg-[#212121] px-1.5 py-1 rounded-xl top-[150px] cursor-pointer select-none  duration-300 items-center flex flex-col text-xl right-[9.7px] ${sidebar ? '' : 'ml-[-133px]'}`}>
         {navArray.map((x, index) => <h1 key={index}>{x}</h1>)}
       </div>
 
