bb83bd88f242c6ceae36777671d9536df364d0b5
Jake <JakeShoffner27@gmail.com>
2023-06-10 10:13:43 -0400

    prelude

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <div className='w-full pt-[70px] h-screen md:pt-[0vh] flex justify-between items-center flex-col text-[#F5EFED]'>
       <div className='grid md:grid-rows-1 h-screen max-w-[1500px] grid-flow-col w-full grid-rows-2 m-[5vh] gap-x-10 gap-y-4 p-5'>
                   
-        <div className='flex flex-col w-full min-w-[600px] justify-center'>
-            <h1 className='text-4xl grad font-[400] text-[] drop-shadow-lg'>Hello, I'm</h1>
-            <h1 className='capitalize font-[700] text-6xl drop-shadow-lg'>Jake Shoffner</h1>
-            <Typed className="text-[#59A5D8] text-4xl font-[500]" strings={['Software engineer...', 'Problem solver...', 'Self-proclaimed comedian...']} typeSpeed={65} loop backDelay={1800} backSpeed={50}/>
+        <div className='flex flex-col w-full md:min-w-[600px] justify-center'>
+            <h1 className='md:text-4xl grad font-[400] text-3xl drop-shadow-lg'>Hello, I'm</h1>
+            <h1 className='capitalize font-[700] md:text-6xl text-5xl drop-shadow-lg'>Jake Shoffner</h1>
+            <Typed className="text-[#59A5D8] md:text-4xl font-[500] text-2xl" strings={['Software engineer...', 'Problem solver...', 'Self-proclaimed comedian...']} typeSpeed={65} loop backDelay={1800} backSpeed={50}/>
         </div>  
 
-        <div className='flex items-center w-full text-xl text-[#d8dad9] justify-center'>
+        <div className='flex items-center w-full text-xl text-[#d8dad9] justify-center md:min-h-full'>
           <div className='bg-[#2d363f] p-6 rounded-xl hover:scale-105 duration-300 bg-opacity-60'>
             <h1 className='font-bold text-4xl text-[#59A5D8] mb-6'>About Me</h1>
             <p className='grad'>
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const MobileNav = () => {
   return (
-    <div className='lg:hidden fixed bg-[#17181a] flex px-6 justify-between items-center w-full h-[70px] text-white'>
+    <div className='lg:hidden fixed bg-[#17181a] flex px-6 justify-between items-center w-full h-[70px] text-white z-10'>
         <h1>Jake Shoffner</h1>
         <div className='duration-300 hover:text-[#b3b3b3] text-[#59A5D8] hover:rotate-90'>
           <h1 className='cursor-pointer'><Icon className='text-[35px]' icon="game-icons:multi-directions" /></h1>
