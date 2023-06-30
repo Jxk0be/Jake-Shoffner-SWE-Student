9ea8e15ccf901a16bc4c8ddc9dcea872d5b2b071
Jake <87618544+Jxk0be@users.noreply.github.com>
2023-06-29 22:27:24 -0400

    refactor: Change the style of the sidebar in order to stay centered for all desktop browser heights (#2)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
   const navArray = nav.split("");
 
   return (
-    <div className={`hidden text-black fixed z-30 drop-shadow-xl bg-[#0b0e10]/90 duration-300 h-screen lg:flex justify-center md:w-[170px] ${sidebar ? 'ml-[-120px]' : ''}`}>
+    <div className={`hidden items-center text-black fixed z-30 drop-shadow-xl bg-[#0b0e10]/90 duration-300 h-screen lg:flex justify-center md:w-[170px] ${sidebar ? 'ml-[-120px]' : ''}`}>
       {/* This is the click-able icon that opens the sidebar */}
 
-      <div className={`absolute top-[450px] ml-[-30px] z-10 left-4 flex justify-between flex-col h-[280px] ${sidebar ? 'opacity-0' : ''}`}>
+      <div className={`mr-[40px] z-10 flex justify-between flex-col h-[280px] ${sidebar ? 'opacity-0' : ''}`}>
 
         <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#59A5DA] hover:bg-white py-2 hover:text-[#59A5DA] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
           <Icon className='text-2xl group-hover:rotate-90 duration-300' icon="material-symbols:verified" />
@@ -40,10 +40,10 @@ const Navbar = () => {
 
       </div>
 
-      {/* This is the click-able "NAVBAR" horizontal text along the sidebar... it also opens the sidebar*/} 
-      <div onClick={() => setSidebar(!sidebar)} className={`absolute bg-[#59A5DA] font-semibold px-1.5 py-1 rounded-xl top-[450px] cursor-pointer select-none duration-150 items-center flex flex-col text-xl right-[9.7px] ${sidebar ? '' : 'mr-[123px] opacity-0 z-0'}`}>
+      {sidebar ? <div onClick={() => setSidebar(!sidebar)} className='bg-[#59A5DA] font-semibold ml-[-79px] px-1.5 py-1 rounded-xl z-40 cursor-pointer select-none duration-150 items-center flex flex-col text-xl'>
         {navArray.map((x, index) => <h1 key={index}>{x}</h1>)}
-      </div>
+      </div> :
+       ``}
 
     </div>
   )
