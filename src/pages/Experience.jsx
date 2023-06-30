eff8534741a06e8eaaa44d7be27266d7d12c904b
Jxk0be <JakeShoffner27@gmail.com>
2023-06-30 12:43:54 -0400

    feat: Adding more to the data file

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import data from '../websiteData.json';
 
 const Experience = () => {
+  console.log(data.projects[0].title);
   return (
-    <div>Experience Page</div>
+    <div>Experience Page
+      
+      {/* Testing */}
+      {/* <img src={data.projects[2].image} alt="asdf" /> */}
+    </div>
+
   )
 }
 
