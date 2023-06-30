26cb9d8e1453867d16ba477ee29dc66b29e2aeaa
Jxk0be <JakeShoffner27@gmail.com>
2023-06-29 22:28:57 -0400

    feat: Added react router and tested with the about page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import Hero from './components/Hero';
+import Hero from './pages/Hero';
+import About from './pages/About';
 import MobileNav from './components/MobileNav';
-import About from './components/About';
+
 
 function App() {
   return (
     <div className='text-[#F5EFED]'>
       <MobileNav />
-
       <div className='flex'>
         <Sidebar />
-        <Hero />
+          <Routes>
+            <Route index element={<Hero />} />
+            <Route path="/home" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
       </div>
-
-      <About />
     </div>
   );
 }
