618bf8e414cc9dc8f62aff20654301f78b001eca
Jxk0be <JakeShoffner27@gmail.com>
2023-06-29 22:46:53 -0400

    feat: Added the rest of the routes and created template pages for them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Hero from './pages/Hero';
 import About from './pages/About';
+import Experience from './pages/Experience';
+import Projects from './pages/Projects';
 import MobileNav from './components/MobileNav';
 
 
@@ -11,11 +13,15 @@ function App() {
       <MobileNav />
       <div className='flex'>
         <Sidebar />
+          <div className='w-full pt-[70px] h-screen md:pt-[0vh] flex justify-between items-center flex-col'>
           <Routes>
             <Route index element={<Hero />} />
             <Route path="/home" element={<Hero />} />
             <Route path="/about" element={<About />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/projects" element={<Projects />} />
           </Routes>
+          </div>
       </div>
     </div>
   );
