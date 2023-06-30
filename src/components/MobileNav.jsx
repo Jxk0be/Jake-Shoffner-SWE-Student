0c5c998ab839ecf7baa00c5566ab204e6851ad9c
Jxk0be <JakeShoffner27@gmail.com>
2023-06-30 14:47:09 -0400

    refactor: Some small changes, nothing much

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
+import { useState } from 'react';
 
 const MobileNav = () => {
+
+  const [mobile, setMobile] = useState(true);
+
   return (
-    <div className='lg:hidden fixed bg-[#eef1f4] flex px-6 justify-between items-center w-full h-[60px] text-black z-10'>
+    <div className={`lg:hidden fixed bg-[#eef1f4] flex px-6 justify-between items-center w-full h-[60px] text-black z-10`}>
         <h1>Jake Shoffner</h1>
-        <div className='duration-300 hover:text-[#b3b3b3] text-[#59A5D8] hover:rotate-90'>
+        <div onClick={() => setMobile(!mobile)} className='text-[#59A5D8]'>
           <h1 className='cursor-pointer'><Icon className='text-[35px]' icon="game-icons:multi-directions" /></h1>
       </div>
     </div>
