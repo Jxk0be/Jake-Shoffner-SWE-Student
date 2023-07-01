1f47568da1b9ba9988b6888320042cf814c6287d
Jxk0be <JakeShoffner27@gmail.com>
2023-07-01 10:39:37 -0400

    feat: Adding grid styles for the projects page, need to redo in TailwindCSS later

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,10 +4,69 @@ import data from '../websiteData.json';
 const Projects = () => {
   console.log(data);
   return (
-    <div className='w-3/4 h-full flex justify-between flex-col text-[#F5EFED] lg:max-w-[1500px] bg-slate-400 lg:ml-[50px] xlg:ml-0'>
-      <div className='grid h-full w-full bg-blue-900 grid-cols-1 lg:grid-cols-2 lg:gap-x-10 lg:gap-y-20'>
-        <h1>Go here when you start back working:</h1>
-        <a href="https://www.youtube.com/watch?v=ZPb10kOQv0c&ab_channel=Mr.WebDesigner" target='_blank' rel="noreferrer">https://www.youtube.com/watch?v=ZPb10kOQv0c&ab_channel=Mr.WebDesigner</a>
+    // <div className='flex justify-between flex-col text-[#F5EFED] lg:max-w-[1500px] bg-slate-400 lg:ml-[50px] xlg:ml-0'>
+    //   <div className='grid h-screen w-full bg-blue-900 grid-flow-col grid-cols-1 lg:grid-cols-2'>
+
+    //     <div className='drop-shadow-lg bg-red-400 flex flex-row justify-between'>
+    //       <div className='w-[400px]'>
+    //         <h1>Epic</h1>
+    //         <p>{data.projects[0].descriptions[0]}</p>
+    //         <p>{data.projects[0].descriptions[1]}</p>
+    //         <p>{data.projects[0].descriptions[2]}</p>
+    //         <p>{data.projects[0].descriptions[3]}</p>
+    //       </div>
+    //       <div className='h-[300px] flex justify-center items-center w-full'>
+    //         <img src={data.projects[0].image} alt="chcess"></img>
+    //       </div>
+    //     </div>
+
+
+    //   </div>
+    // </div>
+
+    <div className="container">
+
+    <h1 className="heading">our services</h1>
+
+      <div className="box-container">
+        <div className="box">
+          <div>
+            <h3>{data.projects[0].title}</h3>
+            <p>- {data.projects[0].descriptions[0]}</p>
+            <p>- {data.projects[0].descriptions[1]}</p>
+            <p>- {data.projects[0].descriptions[2]}</p>
+            <p>- {data.projects[0].descriptions[3]}</p>
+          </div>
+        </div>
+        <div className="box">
+          <img src={data.projects[0].image} alt="" />
+        </div>
+
+        <div className="box">
+          <div>
+            <h3>{data.projects[1].title}</h3>
+            <p>- {data.projects[1].descriptions[0]}</p>
+            <p>- {data.projects[1].descriptions[1]}</p>
+            <p>- {data.projects[1].descriptions[2]}</p>
+          </div>
+        </div>
+        <div className="box test">
+          <img src={data.projects[0].image} alt="" />
+        </div>
+
+        <div className="box">
+          <div>
+            <h3>{data.projects[2].title}</h3>
+            <p>- {data.projects[2].descriptions[0]}</p>
+            <p>- {data.projects[2].descriptions[1]}</p>
+            <p>- {data.projects[2].descriptions[2]}</p>
+          </div>
+        </div>
+        <div className="box">
+          <img src={data.projects[0].image} alt="" />
+        </div>
+
+
       </div>
     </div>
   )
