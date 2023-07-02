5cbc22a855ed270f8a4abbe69b1cf166577c0886
Jxk0be <JakeShoffner27@gmail.com>
2023-07-02 12:36:05 -0400

    style: Modal styling update

--- src/components/Proj.jsx
+++ src/components/Proj.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
 
 const Proj = ({data}) => {
   const [modal, setModal] = useState(false);
@@ -15,8 +16,15 @@ const Proj = ({data}) => {
             {modal ?
                 <>
                    <div className='w-full h-screen absolute left-0 top-0 flex justify-center items-center'>
-                        <div className='bg-black/90 md:w-full md:max-h-[450px] md:max-w-[650px] z-40 p-5 rounded-xl w-screen h-screen fixed top-0 left-0 md:relative'>
-                            <p className='text-lg font-medium'>{data.description}</p>
+                        <div className='bg-black/90 md:w-full md:max-h-[400px] md:max-w-[650px] z-40 p-5 rounded-xl fixed top-0 left-0 md:relative'>
+                            <h1 className='font-bold text-2xl pb-3'>{data.title}</h1>
+                            <p className='text-xl font-small'>{data.description}</p>
+                            <div className='flex flex-row justify-around items-center pt-[10px]'>
+                                <div className='flex flex-row items-center'>
+                                    <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="ri:github-fill" />
+                                    <h1>Github</h1>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
