3abef70827dae6649db3ab50d15e2ddba70ae02d
Jake <87618544+Jxk0be@users.noreply.github.com>
2023-07-04 17:35:07 -0400

    Experience page work (#10)
    * feat: Adding some experience page data and testing formatting
    
    * bruh

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
+import data from '../websiteData.json';
 
 const Experience = () => {
 
   return (
-    <div>Experience Page
-      
-      {/* Testing */}
-      {/* <img src={data.projects[2].image} alt="asdf" /> */}
-    </div>
+    <div className='w-full h-screen md:h-3/4 flex justify-center flex-col items-center bg-slate-500 md:max-w-[1500px] max-h-[1080px] md:ml-[80px] md:mr-[80px] lg:mx-[85px]'>
+      <div className='h-auto max-h-[600px] bg-red-500 w-7/12 flex items-center my-2 flex-col'>
+        <div className='w-full flex justify-between items-center px-3 flex-col md:flex-row'>
+          <h1>{data.experiences[0].jobTitle}</h1>
+          <h1>{data.experiences[0].location}</h1>
+        </div>
 
+        <div className='w-full flex justify-between items-center px-3'>
+          <h1>{data.experiences[0].company}</h1>
+          <h1>{data.experiences[0].date}</h1>
+        </div>
+
+        <div>
+          {data.experiences[0].descriptions.map((p) => {
+            return <p>- {p}</p>
+          })}
+        </div>
+      </div>
+
+      <div className='h-auto max-h-[600px] bg-red-500 w-7/12 flex items-center my-2 flex-col'>
+        <div className='w-full flex justify-between items-center px-3'>
+          <h1>{data.experiences[1].jobTitle}</h1>
+          <h1>{data.experiences[1].location}</h1>
+        </div>
+
+        <div className='w-full flex justify-between items-center px-3'>
+          <h1>{data.experiences[1].company}</h1>
+          <h1>{data.experiences[1].date}</h1>
+        </div>
+      </div>
+
+      <div className='h-auto max-h-[600px] bg-red-500 w-7/12 flex items-center my-2 flex-col'>
+        <div className='w-full flex justify-between items-center px-3'>
+          <h1>{data.experiences[2].jobTitle}</h1>
+          <h1>{data.experiences[2].location}</h1>
+        </div>
+
+        <div className='w-full flex justify-between items-center px-3'>
+          <h1>{data.experiences[2].company}</h1>
+          <h1>{data.experiences[2].date}</h1>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
