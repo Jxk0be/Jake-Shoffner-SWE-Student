112fb143486b76e90ff45416938816e41243d377
Jxk0be <JakeShoffner27@gmail.com>
2023-07-04 13:49:20 -0400

    bruh

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
   return (
     <div className='w-full h-screen md:h-3/4 flex justify-center flex-col items-center bg-slate-500 md:max-w-[1500px] max-h-[1080px] md:ml-[80px] md:mr-[80px] lg:mx-[85px]'>
       <div className='h-auto max-h-[600px] bg-red-500 w-7/12 flex items-center my-2 flex-col'>
-        <div className='w-full flex justify-between items-center px-3'>
+        <div className='w-full flex justify-between items-center px-3 flex-col md:flex-row'>
           <h1>{data.experiences[0].jobTitle}</h1>
           <h1>{data.experiences[0].location}</h1>
         </div>
@@ -14,6 +14,12 @@ const Experience = () => {
         <div className='w-full flex justify-between items-center px-3'>
           <h1>{data.experiences[0].company}</h1>
           <h1>{data.experiences[0].date}</h1>
+        </div>
+
+        <div>
+          {data.experiences[0].descriptions.map((p) => {
+            return <p>- {p}</p>
+          })}
         </div>
       </div>
 
