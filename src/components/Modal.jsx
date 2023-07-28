9c223121052da896889a35f535fd914f7a96cc8c
Jxk0be <JakeShoffner27@gmail.com>
2023-07-27 23:05:29 -0400

    feat: Modal updates

---
+++ src/components/Modal.jsx
@@ -0,0 +1,129 @@
+import Typed from 'react-typed';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+
+const Modal = ({data, onClose}) => {
+  const [bsSelector, setBsSelector] = useState(true);
+  const [feSelector, setFeSelector] = useState(false);
+  const [langSelector, setLangSelector] = useState(false);
+  const [toolSelector, setToolSelector] = useState(false);
+
+  /* This code should be refactored later, I don't like how repeated this is.
+     Need to research if there's a better way to do this, but this is off
+     the ole' brain.
+  */
+  const bsCleanup = () => {
+    setBsSelector(true);
+    setFeSelector(false);
+    setLangSelector(false);
+    setToolSelector(false);
+  }
+
+  const feCleanup = () => {
+    setFeSelector(true);
+    setBsSelector(false);
+    setLangSelector(false);
+    setToolSelector(false);
+  }
+
+  const langCleanup = () => {
+    setLangSelector(true);
+    setFeSelector(false);
+    setBsSelector(false);
+    setToolSelector(false);
+  }
+
+  const toolCleanup = () => {
+    setToolSelector(true);
+    setFeSelector(false);
+    setBsSelector(false);
+    setLangSelector(false);
+  }
+
+  return (
+    <>
+    <div className='flex justify-center items-center z-30 fixed md:w-[600px] w-full h-1/2 md:h-auto'>
+      <div className="md:h-auto relative md:w-full w-full h-auto bg-[#6a27b2] rounded-lg flex justify-center items-center flex-col py-5">
+        <div className='absolute right-0 top-0 pr-3 pt-3 cursor-pointer' onClick={onClose}>
+          <Icon className='text-[30px] hover:text-[#ed08be] duration-150' icon="carbon:close-outline" />
+        </div>
+        <h1 className="text-3xl font-bold">{data.MoreInfo.title}</h1>
+
+        <Typed className="text-[#ed08be] font-semibold text-xl" strings={['Software engineer...', 'Senior in Computer Science...']} typeSpeed={65} loop backDelay={2000} backSpeed={50}/>
+        
+        <div className="flex justify-between items-center text-center w-full pb-5 md:px-5 text-[14px] whitespace-nowrap">
+          <p className='w-1/3'>Knoxville, TN</p>
+          <a className="w-1/3 underline" href="mailto:jshoffn3@vols.utk.edu">jshoffn3@vols.utk.edu</a>
+          <p className='w-1/3'>978-2087</p>
+        </div>
+
+        <h1 className='text-2xl font-bold'>{data.MoreInfo.subTitle}</h1>
+        <div className='w-9/12 h-[1px] bg-red-50 mb-3'>&nbsp;</div>
+
+        <div className='md:w-9/12 w-full px-2 flex flex-row justify-between items-center h-[145px]'>
+          <div className='h-full whitespace-nowrap min-w-4/12 text-left'>
+            <div className={`duration-300 px-2 cursor-pointer my-2 text-xl md:hover:scale-105 text-center font-semibold noSelect ${bsSelector ? 'bg-[#ed08be] scale-105 rounded-md' : ''}`} onClick={() => bsCleanup()}>
+              <h1>Basic Skills</h1>
+            </div>
+            <div className={`duration-300 cursor-pointer my-2 text-xl md:hover:scale-105 text-center font-semibold noSelect ${feSelector ? 'bg-[#ed08be] scale-105 rounded-md' : ''}`} onClick={() => feCleanup()}>
+              <h1>Front-End</h1>
+            </div>
+            <div className={`duration-300 cursor-pointer my-2 text-xl md:hover:scale-105 text-center font-semibold noSelect ${langSelector ? 'bg-[#ed08be] scale-105 rounded-md' : ''}`} onClick={() => langCleanup()}>
+              <h1>Languages</h1>
+            </div>
+            <div className={`duration-300 cursor-pointer my-2 text-xl md:hover:scale-105 text-center font-semibold noSelect ${toolSelector ? 'bg-[#ed08be] scale-105 rounded-md' : ''}`} onClick={() => toolCleanup()}>
+              <h1>Tooling</h1>
+            </div>            
+          </div>
+
+          <div className='w-full h-full text-lg font-bold flex ml-5 pl-3 pt-2 bg-black/20 items-start flex-col rounded-xl'>
+            {bsSelector ?
+              data.MoreInfo.overallSkills.map((skills, index) => {
+                return (
+                  <h1 key={index}>- {skills}</h1>
+                );
+              })
+            :
+            ''
+            }
+
+            {feSelector ?
+              data.MoreInfo.techSkills.frontend.map((skills, index) => {
+                return(
+                  <h1 key={index}>- {skills}</h1>
+                );
+              }) 
+            :
+            ''
+            }
+
+            {langSelector ?
+              data.MoreInfo.techSkills.languages.map((skills, index) => {
+                return(
+                  <h1 key={index}>- {skills}</h1>
+                );
+              })
+            :
+            ''
+            }
+
+            {toolSelector ?
+              data.MoreInfo.techSkills.toolsOthers.map((skills, index) => {
+                return(
+                  <h1 key={index}>- {skills}</h1>
+                );
+              })
+            :
+            ''
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div onClick={onClose} className='h-screen w-full bg-black/40 fixed z-20'></div>
+    </>
+  )
+}
+
+export default Modal
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,41 +10,41 @@ const Navbar = () => {
 
   return (
     <>
-      <div className={`hidden items-center text-[#b3b3b3] text-[17px] font-bold fixed z-50 drop-shadow-xl bg-[#121212] duration-300 h-screen lg:flex justify-center md:w-[170px] ${sidebar ? 'ml-[-120px]' : ''}`}>
+      <div className={`hidden fluidBG items-center text-[#ed08be] text-[17px] font-bold fixed z-50 drop-shadow-xl bg-[#1a191a] duration-300 h-screen lg:flex justify-center md:w-[170px] ${sidebar ? 'ml-[-120px]' : ''}`}>
         {/* This is the click-able icon that opens the sidebar */}
 
         <div className={`mr-[40px] z-10 flex noSelect justify-between flex-col h-[300px] ${sidebar ? 'opacity-0' : ''}`}>
 
           <Link to="/home">
-            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-white py-2 hover:text-[#212121] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#FAF9F6] hover:bg-[#ed08be] py-2 hover:text-[#FAF9F6] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
               <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="ion:home" />          
               <h1>Home</h1>
             </div>
           </Link>
           
           <Link to="/experience">
-            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-white py-2 hover:text-[#212121] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#FAF9F6] hover:bg-[#ed08be] py-2 hover:text-[#FAF9F6] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
               <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="cryptocurrency:xp" />
               <h1>Experience</h1>
             </div>
           </Link>
 
           <Link to="/projects">
-            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-white py-2 hover:text-[#212121] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#FAF9F6] hover:bg-[#ed08be] py-2 hover:text-[#FAF9F6] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
               <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="file-icons:ionic-project" />
               <h1>Projects</h1>
             </div>
           </Link>
 
           <a href="https://github.com/Jxk0be" target='_blank' rel="noreferrer">
-            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-white py-2 hover:text-[#212121] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#FAF9F6] hover:bg-[#ed08be] py-2 hover:text-[#FAF9F6] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
               <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="ri:github-fill" />
               <h1>GitHub</h1>
             </div>
           </a>
 
           <a href="https://www.linkedin.com/in/jacobshoffner/" target='_blank' rel="noreferrer">
-            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#212121] hover:bg-white py-2 hover:text-[#212121] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
+            <div onClick={() => setSidebar(!sidebar)} className='flex group justify-between w-[160px] bg-[#FAF9F6] hover:bg-[#ed08be] py-2 hover:text-[#FAF9F6] rounded-xl pr-2 pl-5 hover:scale-105 cursor-pointer duration-300'>
               <Icon className='text-3xl group-hover:rotate-90 duration-300' icon="uil:linkedin" />
               <h1>Linkedin</h1>
             </div>
@@ -53,7 +53,7 @@ const Navbar = () => {
         </div>
 
         {sidebar ? 
-        <div onClick={() => setSidebar(!sidebar)} className='bg-[#212121] hover:bg-white hover:text-[#212121] font-semibold ml-[-79px] px-1.5 py-1 rounded-xl z-40 cursor-pointer select-none duration-150 items-center flex flex-col text-xl group'>
+        <div onClick={() => setSidebar(!sidebar)} className='hover:text-[#ed08be] text-[#FAF9F6] font-semibold ml-[-79px] px-1.5 py-1 rounded-xl z-40 cursor-pointer select-none duration-150 items-center flex flex-col text-xl group'>
           {navArray.map((x, index) => <h1 key={index}>{x}</h1>)}
         </div> :
         ``}
